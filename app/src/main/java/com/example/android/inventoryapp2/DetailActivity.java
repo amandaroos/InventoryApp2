55029d337fc0ecb4e52bc0f01dc3c9a0b929ecbf
Amanda Farrell <bluepandadev@gmail.com>
2018-10-19 15:54:42 -0600

    Implement sale button in MainActivity and Quantity increase/decrease buttons in DetailActivity

--- app/src/main/java/com/example/android/inventoryapp2/DetailActivity.java
+++ app/src/main/java/com/example/android/inventoryapp2/DetailActivity.java
@@ -1,18 +1,25 @@
 package com.example.android.inventoryapp2;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.inventoryapp2.data.InventoryContract.InventoryEntry;
 
@@ -38,6 +45,9 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     private EditText mSupplierNameEditText;
     private EditText mSupplierPhoneNumberEditText;
 
+    private Button mIncreaseInventoryButton;
+    private Button mDecreaseInventoryButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +70,52 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         mSupplierNameEditText = (EditText) findViewById(R.id.edit_text_product_supplier_name);
         mSupplierPhoneNumberEditText = (EditText) findViewById(R.id.edit_text_product_supplier_phone_number);
 
+        mIncreaseInventoryButton = (Button) findViewById(R.id.button_increase_inventory);
+        mDecreaseInventoryButton = (Button) findViewById(R.id.button_decrease_inventory);
+
         mProductNameEditText.setOnTouchListener(mTouchListener);
         mProductPriceEditText.setOnTouchListener(mTouchListener);
         mProductQuantityEditText.setOnTouchListener(mTouchListener);
         mSupplierNameEditText.setOnTouchListener(mTouchListener);
         mSupplierPhoneNumberEditText.setOnTouchListener(mTouchListener);
+
+        //Set default values
+        mProductPriceEditText.setText("0");
+        mProductQuantityEditText.setText("0");
+
+        mIncreaseInventoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean increase = true;
+                changeProductQuantity(increase);
+            }
+        });
+
+        mDecreaseInventoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //If increase is false, the quantity will decrease
+                boolean increase = false;
+                changeProductQuantity(increase);
+            }
+        });
+    }
+
+    public void changeProductQuantity(boolean increase) {
+
+        int productQuantityInteger = Integer.parseInt(mProductQuantityEditText.getText().toString());
+
+        //if increase is true, increase quantity
+        //otherwise, if it is greater than 0, decrease the quantity
+        if (increase) {
+            productQuantityInteger += 1;
+        } else {
+            if (productQuantityInteger > 0) {
+                productQuantityInteger -= 1;
+            }
+        }
+
+        mProductQuantityEditText.setText(String.valueOf(productQuantityInteger));
     }
 
     @Override
@@ -73,7 +124,102 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                saveProduct();
+                // Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                //Open dialog to warn user of the deletion
+                //TODO showDeleteConfirmationDialog();
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // If the product hasn't changed, continue with navigating up to parent activity
+                // which is the {@link MainActivity}.
+                if (!mProductHasChanged) {
+                    NavUtils.navigateUpFromSameTask(DetailActivity.this);
+                    return true;
+                }
 
+                // Otherwise if there are unsaved changes, setup a dialog to warn the user.
+                // Create a click listener to handle the user confirming that
+                // changes should be discarded.
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // User clicked "Discard" button, navigate to parent activity.
+                                NavUtils.navigateUpFromSameTask(DetailActivity.this);
+                            }
+                        };
+
+                // Show a dialog that notifies the user they have unsaved changes
+                //TODO showUnsavedChangesDialog(discardButtonClickListener);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Get user input from detail activity and update the product in database
+    private void saveProduct() {
+        //Get product data from user input
+        String productNameString = mProductNameEditText.getText().toString().trim();
+        String productPriceString = mProductPriceEditText.getText().toString().trim();
+        String productQuantityString = mProductQuantityEditText.getText().toString().trim();
+        String supplierNameString = mSupplierNameEditText.getText().toString().trim();
+        String supplierPhoneNumberString = mSupplierPhoneNumberEditText.getText().toString().trim();
+
+        //Prevent crash when saving a blank detail page
+        //Check if this is supposed to be a new product and check if all the fields are blank
+        if (currentProductURi == null &&
+                TextUtils.isEmpty(productNameString) &&
+                TextUtils.isEmpty(productPriceString) &&
+                TextUtils.isEmpty(productQuantityString) &&
+                TextUtils.isEmpty(supplierNameString) &&
+                TextUtils.isEmpty(supplierPhoneNumberString)) {
+            //Since no fields were modified, we can return early without creating a new product.
+            return;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(InventoryEntry.COLUMN_PRODUCT_NAME, productNameString);
+        values.put(InventoryEntry.COLUMN_PRODUCT_PRICE, productPriceString);
+        values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, productQuantityString);
+        values.put(InventoryEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
+        values.put(InventoryEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneNumberString);
+
+        if (currentProductURi != null) {
+            //pass the update product information to the content resolver
+            int rowsAffected = getContentResolver().update(currentProductURi, values, null, null);
+
+            //Show a toast message depending on whether or not hte update was successful
+            if (rowsAffected == 0) {
+                Toast.makeText(this, R.string.detail_activity_update_product_failed,
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.detail_activity_update_product_successful,
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            //Insert the new row using the InventoryProvider
+            Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
+
+            if (newUri == null) {
+                Toast.makeText(this, R.string.detail_activity_insert_product_failed,
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.detail_activity_insert_product_successful,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
@@ -97,7 +243,7 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         //Check to see if the Cursor is empty
-        if (cursor.getCount() > 0){
+        if (cursor.getCount() > 0) {
             //Set Cursor to the 0th position
             cursor.moveToFirst();
 
--- app/src/main/java/com/example/android/inventoryapp2/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp2/InventoryCursorAdapter.java
@@ -1,13 +1,18 @@
 package com.example.android.inventoryapp2;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import com.example.android.inventoryapp2.data.InventoryContract;
 import com.example.android.inventoryapp2.data.InventoryContract.InventoryEntry;
 
 public class InventoryCursorAdapter extends CursorAdapter {
@@ -36,17 +41,19 @@ public class InventoryCursorAdapter extends CursorAdapter {
     /**
      * This method binds the product data to the given list item layout
      *
-     * @param view Existing view, returned earliet by newView() method
+     * @param view    Existing view, returned earliet by newView() method
      * @param context The app context
-     * @param cursor The cursor is already moved to the correct row
+     * @param cursor  The cursor is already moved to the correct row
      */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
 
         //Find individual views that we want to modify in the list item layout
         TextView productNameTextView = (TextView) view.findViewById(R.id.text_view_product_name);
-        TextView productPriceTextView = (TextView) view.findViewById(R.id.text_view_product_name);
-        TextView productQuantityTextView = (TextView) view.findViewById(R.id.text_view_product_name);
+        TextView productPriceTextView = (TextView) view.findViewById(R.id.text_view_product_price);
+        TextView productQuantityTextView = (TextView) view.findViewById(R.id.text_view_product_quantity);
+
+        Button saleButton = (Button) view.findViewById(R.id.button_sale);
 
         //Find the columns of the product attributes
         int productNameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_NAME);
@@ -56,11 +63,38 @@ public class InventoryCursorAdapter extends CursorAdapter {
         //Read the product attributes from the Cursor for the current product
         String productName = cursor.getString(productNameColumnIndex);
         String productPrice = cursor.getString(productPriceColumnIndex);
-        String productQuantity = cursor.getString(productQuantityColumnIndex);
+        final String productQuantity = cursor.getString(productQuantityColumnIndex);
 
         //Update the TextViews with the attributes for the current product
         productNameTextView.setText(productName);
         productPriceTextView.setText(productPrice);
         productQuantityTextView.setText(productQuantity);
+
+        //Get the current product URI
+        int productIdColumnIndex = cursor.getColumnIndex(InventoryEntry._ID);
+        int productId = cursor.getInt(productIdColumnIndex);
+        final Uri currentProductUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, productId);
+
+        //Set onClick Listener on sale button
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int productQuantityInteger = Integer.parseInt(productQuantity);
+
+                //Decrease the quantity of the product by 1
+                if (productQuantityInteger > 0) {
+                    productQuantityInteger -= 1;
+                } else {
+                    productQuantityInteger = 0;
+                }
+                ContentValues values = new ContentValues();
+                values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, productQuantityInteger);
+
+                //Pass the Content Resolver the updated product quantity
+                int rowsAffected = context.getContentResolver()
+                        .update(currentProductUri, values, null, null);
+
+            }
+        });
     }
 }
